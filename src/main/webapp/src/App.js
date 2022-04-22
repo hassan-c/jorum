b008d1fad91606c6b155c3054263484cc5137984
Hassan C <maniac_9989@hotmail.co.uk>
2022-04-22 18:44:43 +0100

    Super basic frontend sorted for now - single button that fetches users on click.

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  async function handleClick() {
+    const users = await fetch('api/v1/users');
+    setUsers(await users.json());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {users.map(user => {
+          return <li key={user.id}>{JSON.stringify(user)}</li>
+        })}
+      </ul>
+      <button onClick={handleClick}>List all users</button>
     </div>
   );
 }
