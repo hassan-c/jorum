b6a09b549433ef3326043b69b9bf9b3bf4a3d531
Hassan C <maniac_9989@hotmail.co.uk>
2022-05-07 20:14:40 +0100

    Revert previous commit - we do actually need a Category controller/repository/service...

---
+++ src/main/java/com/hasc/jorum/category/CategoryController.java
@@ -0,0 +1,18 @@
+package com.hasc.jorum.category;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/category")
+@AllArgsConstructor
+public class CategoryController {
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getCategories() {
+        return categoryService.getCategories();
+    }
+}
---
+++ src/main/java/com/hasc/jorum/category/CategoryService.java
@@ -0,0 +1,16 @@
+package com.hasc.jorum.category;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+}
